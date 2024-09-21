5b1d8604a352162dd834d1621dab610d85462fff
kerolos Magdy <kero.magdy2000@gmail.com>
2024-09-21 17:04:12 +0300

    feat: 🎸 add NotFound page

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,12 +1,41 @@
-import { Box, Typography } from '@mui/material';
+import { Box, Button, useTheme } from '@mui/material';
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Routes } from 'src/constant';
+
+import NotFoundIcon from '../assets/not-found.svg';
 
 export interface NotFoundProps {}
 
 export const NotFound: React.FC<NotFoundProps> = () => {
+	const theme = useTheme();
+	const navigate = useNavigate();
+
 	return (
-		<Box>
-			<Typography>NotFound</Typography>
+		<Box
+			sx={{
+				alignItems: 'center',
+				display: 'flex',
+				flexDirection: 'column',
+				flexGrow: 1,
+				height: '100%',
+				justifyContent: 'center',
+			}}
+		>
+			<Box
+				component='img'
+				src={NotFoundIcon}
+				sx={{
+					aspectRatio: '1/1',
+					height: theme.spacing(50),
+				}}
+			/>
+			<Button
+				onClick={() => navigate(Routes.Home)}
+				variant='contained'
+			>
+				Go Home
+			</Button>
 		</Box>
 	);
 };
