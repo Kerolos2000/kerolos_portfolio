26e0bbef1dbafd3bcb3749bc65844f5329448ad0
kerolos Magdy <kero.magdy2000@gmail.com>
2025-01-17 21:00:16 +0200

    feat: 🎸 save

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,46 @@
+/* eslint-disable perfectionist/sort-objects */
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import compression from 'vite-plugin-compression';
+import viteImagemin from 'vite-plugin-imagemin';
 
 export default defineConfig({
-	plugins: [react()],
+	build: {
+		minify: 'esbuild',
+		rollupOptions: {
+			output: {
+				manualChunks: {
+					vendor: ['@emotion/react', '@emotion/styled'],
+					mui: ['@mui/icons-material', '@mui/material'],
+					other: ['react', 'react-dom', 'react-router-dom'],
+				},
+			},
+		},
+		target: 'esnext',
+	},
+	plugins: [
+		react(),
+		compression({
+			algorithm: 'brotliCompress',
+			ext: '.br',
+			threshold: 10240,
+		}),
+		viteImagemin({
+			gifsicle: { optimizationLevel: 7 },
+			mozjpeg: { quality: 80 },
+			optipng: { optimizationLevel: 7 },
+			svgo: {
+				plugins: [{ name: 'removeViewBox' }, { name: 'removeDimensions' }],
+			},
+			webp: { quality: 80 },
+		}),
+	],
 	resolve: {
 		alias: {
-			src: new URL('./src', import.meta.url).pathname,
+			src: '/src',
 		},
+	},
+	server: {
+		hmr: true,
 	},
 });
