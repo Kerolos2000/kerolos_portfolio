87bd13896295cdf61726a244000b25239937df82
kerolos Magdy <kero.magdy2000@gmail.com>
2025-03-07 20:03:17 +0200

    feat: 🎸 add vite-plugin-pwa

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,65 @@ import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import { chunkSplitPlugin } from 'vite-plugin-chunk-split';
 import viteImagemin from 'vite-plugin-imagemin';
+import { VitePWA, VitePWAOptions } from 'vite-plugin-pwa';
 import sitemap from 'vite-plugin-sitemap';
 
 const routes = [
 	{ priority: 1.0, url: '/' },
 	{ priority: 0.8, url: '/projects' },
 ];
+
+const manifestForPlugin: Partial<VitePWAOptions> = {
+	devOptions: {
+		enabled: true,
+	},
+	includeAssets: [
+		'favicon.ico',
+		'favicon-16x16.png',
+		'favicon-32x32.png',
+		'apple-touch-icon.png',
+	],
+	manifest: {
+		background_color: '#F7F8FA',
+		description: 'An app for Kerolos Portfolio.',
+		display: 'standalone',
+		icons: [
+			{
+				sizes: '192x192',
+				src: './assets/android-chrome-192x192.png',
+				type: 'image/png',
+			},
+			{
+				sizes: '512x512',
+				src: './assets/android-chrome-512x512.png',
+				type: 'image/png',
+			},
+			{
+				sizes: '180x180',
+				src: './assets/apple-touch-icon.png',
+				type: 'image/png',
+			},
+			{
+				sizes: '16x16',
+				src: './assets/favicon-16x16.png',
+				type: 'image/png',
+			},
+			{
+				sizes: '32x32',
+				src: './assets/favicon-32x32.png',
+				type: 'image/png',
+			},
+		],
+		name: 'Kerolos Magdy',
+		orientation: 'portrait',
+		scope: '/',
+		short_name: 'Kerolos Magdy',
+		start_url: '/',
+		theme_color: '#F7F8FA',
+	},
+	minify: true,
+	registerType: 'autoUpdate',
+};
 
 export default defineConfig({
 	build: {
@@ -75,6 +128,7 @@ export default defineConfig({
 			readable: true,
 			robots: [{ allow: '/', userAgent: '*' }],
 		}),
+		VitePWA(manifestForPlugin),
 	],
 	resolve: {
 		alias: {
